6f153d11a6db39f970bb2a3afdab355e435b437c
Pedro Lucas Santos <plsantosbrito12@gmail.com>
2026-03-16 20:46:02 -0300

    feat: add exception handling

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-
+from fastapi.exceptions import RequestValidationError
 from app.routes.risk_routes import router
-
+from app.exceptions import validation_exception_handler
 
 app = FastAPI()
 
@@ -10,3 +10,5 @@ app.include_router(router, prefix="/api/v1")
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
